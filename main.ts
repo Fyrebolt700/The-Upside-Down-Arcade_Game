72a57bf9805520dd3cf30d548991aef87bb542ca
Rithanya <83367888+Fyrebolt700@users.noreply.github.com>
2026-07-04 17:02:37 -0400

    added levels

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+namespace SpriteKind {
+    export const goal = SpriteKind.create()
+}
+function end_screen () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+    tiles.setCurrentTilemap(tilemap`level6`)
+}
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     Normal_World()
 })
@@ -11,10 +19,13 @@ function Upside_Down () {
         scene.setBackgroundColor(5)
         mySprite.ay = -200
         mySprite.setPosition(mySprite.x, mySprite.y + 100)
-        enemy_movement(7, 13)
+        enemy_position2(7, 13)
     }
 }
-function enemy_movement (col: number, row: number) {
+function enemy_movement () {
+	
+}
+function enemy_position2 (col: number, row: number) {
     ENEMY = sprites.create(img`
         . . 5 4 2 2 2 2 2 2 2 4 5 . . . 
         . . 5 4 2 2 2 2 2 2 2 4 5 . . . 
@@ -33,12 +44,35 @@ function enemy_movement (col: number, row: number) {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Enemy)
-    position(ENEMY, 7, 13)
     ENEMY.ay = -200
-    ENEMY.setVelocity(-20, 0)
+    if (current_level == 1) {
+        ENEMY.setVelocity(-20, 0)
+        position(ENEMY, 7, 13)
+    }
+    if (current_level == 2) {
+        ENEMY.setVelocity(-20, 0)
+        position(ENEMY, 7, 13)
+    }
+    if (current_level == 3) {
+        ENEMY.setVelocity(-20, 0)
+        position(ENEMY, 7, 13)
+    }
 }
 function position (sprite: Sprite, col: number, row: number) {
     sprite.setPosition(col * 16 + 8, row * 16 + 8)
+}
+function next_level () {
+    if (current_level == 1) {
+        current_level += 1
+        load_level_2()
+    }
+    if (current_level == 2) {
+        current_level += 1
+        load_level_3()
+    }
+    if (current_level == 3) {
+        end_screen()
+    }
 }
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (isUpsideDown && freeze_counter == 0) {
@@ -58,74 +92,250 @@ function Normal_World () {
         mySprite.setPosition(mySprite.x, mySprite.y - 100)
     }
 }
+function load_level_3 () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+    freeze_counter = 0
+    mySprite_img_upside_down = img`
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f 1 f f 1 f f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . f f . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `
+    mySprite_img_normal = img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f 1 f f 1 f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        `
+    tiles.setCurrentTilemap(tilemap`level5`)
+    scene.setBackgroundColor(3)
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f 1 f f 1 f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        `, SpriteKind.Player)
+    position(mySprite, 0, 4)
+    position(goalpost, 15, 4)
+    mySprite.ay = 200
+    controller.moveSprite(mySprite, 100, 0)
+    scene.cameraFollowSprite(mySprite)
+    Normal_World()
+}
+function load_level_2 () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+    freeze_counter = 0
+    mySprite_img_upside_down = img`
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f 1 f f 1 f f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . f f . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `
+    mySprite_img_normal = img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f 1 f f 1 f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        `
+    tiles.setCurrentTilemap(tilemap`level2`)
+    scene.setBackgroundColor(3)
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f 1 f f 1 f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        `, SpriteKind.Player)
+    position(mySprite, 0, 4)
+    position(goalpost, 15, 4)
+    mySprite.ay = 200
+    controller.moveSprite(mySprite, 100, 0)
+    scene.cameraFollowSprite(mySprite)
+    Normal_World()
+}
+function reset_level () {
+    if (current_level == 1) {
+        load_level_1()
+    }
+    if (current_level == 2) {
+        load_level_2()
+    }
+    if (current_level == 3) {
+        load_level_3()
+    }
+}
+function load_level_1 () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+    freeze_counter = 0
+    mySprite_img_upside_down = img`
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f 1 f f 1 f f . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . . f f . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `
+    mySprite_img_normal = img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f 1 f f 1 f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        `
+    tiles.setCurrentTilemap(tilemap`level1`)
+    scene.setBackgroundColor(3)
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f . . . . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f 1 f f 1 f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        `, SpriteKind.Player)
+    position(mySprite, 0, 4)
+    position(goalpost, 15, 4)
+    mySprite.ay = 200
+    controller.moveSprite(mySprite, 100, 0)
+    scene.cameraFollowSprite(mySprite)
+    Normal_World()
+}
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    game.reset()
+    reset_level()
 })
+let mySprite_img_normal: Image = null
+let freeze_counter = 0
 let ENEMY: Sprite = null
+let mySprite_img_upside_down: Image = null
 let isUpsideDown = false
 let mySprite: Sprite = null
-let mySprite_img_normal: Image = null
-let mySprite_img_upside_down: Image = null
-let freeze_counter = 0
-freeze_counter = 0
-mySprite_img_upside_down = img`
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f 1 f f 1 f f . . . . . 
-    . . . . f f f f f f . . . . . . 
-    . . . . f f f f f f . . . . . . 
-    . . . . . f f . . . . . . . . . 
+let current_level = 0
+let goalpost: Sprite = null
+goalpost = sprites.create(img`
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `
-mySprite_img_normal = img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . f f . . . . . . . . . 
-    . . . . f f f f f f . . . . . . 
-    . . . . f f f f f f . . . . . . 
-    . . . . f 1 f f 1 f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    `
-tiles.setCurrentTilemap(tilemap`level1`)
-scene.setBackgroundColor(3)
-mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . f f . . . . . . . . . 
-    . . . . f f f f f f . . . . . . 
-    . . . . f f f f f f . . . . . . 
-    . . . . f 1 f f 1 f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    . . . . f f f f f f f . . . . . 
-    `, SpriteKind.Player)
-position(mySprite, 0, 4)
-mySprite.ay = 200
-controller.moveSprite(mySprite, 100, 0)
-scene.cameraFollowSprite(mySprite)
-Normal_World()
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . . 2 2 2 2 . . . . . . . 
+    . . . . 2 2 2 2 2 . . . . . . . 
+    . . . 2 2 2 2 2 2 . . . . . . . 
+    . 2 2 2 2 2 2 2 2 . . . . . . . 
+    2 2 2 2 2 2 2 2 2 . . . . . . . 
+    . . . . . . . 4 4 . . . . . . . 
+    . . . . . . . 4 4 . . . . . . . 
+    . . . . . . . 4 4 . . . . . . . 
+    . . . . . . . 4 4 . . . . . . . 
+    . . . . . . . 4 4 . . . . . . . 
+    . . . . . . . 4 4 . . . . . . . 
+    . . . . . . . 4 4 . . . . . . . 
+    . . . . . . . 4 4 . . . . . . . 
+    . . . . . . . 4 4 . . . . . . . 
+    . . . . . . . 4 4 . . . . . . . 
+    `, SpriteKind.goal)
+current_level = 1
+load_level_1()
+if (mySprite.overlapsWith(goalpost)) {
+    next_level()
+}
